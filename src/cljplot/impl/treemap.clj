dff1e81173b15aaf51e6135cba88dca4edac391a
tom <tom@that-place>
2019-06-25 19:41:00 -0500

    Got basic project visual working

--- src/cljplot/impl/treemap.clj
+++ src/cljplot/impl/treemap.clj
@@ -14,22 +14,88 @@
 (set! *unchecked-math* :warn-on-boxed)
 (m/use-primitive-operators)
 
+(defprotocol IBox
+  (as-box [o] "returns a map of {x y w h label}"))
+
+(extend-protocol IBox
+  clojure.lang.PersistentArrayMap
+  (as-box [o] o)
+  clojure.lang.PersistentHashMap
+  (as-box [o] o)
+  clojure.lang.PersistentVector
+  (as-box [o]
+    (case (count o)
+      4 (zipmap [:x :y :w :h] o)
+      5 (zipmap [:x :y :w :h :label] o)
+      (throw (ex-info "expected [x y w h label?] for box description" {:input o}))))
+  clojure.lang.ISeq
+  (as-box [o]
+    (as-box (vec o))))
+
 ;;we'd like to render a node.
 ;;and its children can be a different color?
 
+(defn box-extents [xs]
+  (let [[xmin xmax ymin ymax]
+        (reduce (fn [[xmin xmax ymin ymax] {:keys [x y w h]}]
+                  [(min xmin x (+ x w))
+                   (max xmax x (+ x w))
+                   (min ymin y (+ y h))
+                   (max ymax y (+ y h))])
+                [0 0 0 0]
+                (map as-box xs))]
+    {:x [:numerical [xmin xmax]]
+     :y [:numerical [ymin ymax]]}))
+  
+
+(defmethod data-extent :boxes [_ data _]
+  (box-extents data))
+
+
+#_(defmethod render-graph :label [_ {:keys [s pos shift-y]} {:keys [font font-size font-style color]} {:keys [^int w ^int h orientation] :as chart-data}]
+  (let [fix-orientation (assoc chart-data :orientation (case orientation
+                                                         :left :right
+                                                         :top :bottom
+                                                         orientation))]
+    (do-graph fix-orientation false
+      (when font (set-font c font))
+      (when font-size
+        (if font-style
+          (set-font-attributes c font-size font-style)
+          (set-font-attributes c font-size)))
+
+      (when (= orientation :right)
+        (-> c
+            (translate (/ w 2) (/ h 2))
+            (rotate m/PI)
+            (translate (- (/ w 2)) (- (/ h 2)))))
+      (-> c
+          (translate (/ w 2) shift-y) 
+          (translate pos)
+          (set-color color)
+          (text s 0 0 :center)))))
 
 ;;we're really just rendering rectangles at the end.
 ;;working backwards from there..
-(defmethod render-graph :tree-map [_ data {:keys [color stroke size shape] :as conf}
+;;note: we can trivially extend this to be an arbitary renderer.
+(defmethod render-graph :boxes [_ data {:keys [color stroke size shape outline] :as conf}
                                    {:keys [w h x y] :as chart-data}]
   (let [scale-x (partial (:scale x) 0 w)
-        scale-y (partial (:scale y) 0 h)]
-    (do-graph chart-data :highest 
-      (let [coords (mapv (fn [[x y w h]]
-                           (into (transform c (scale-x x) (scale-y y)) [w h])) data)]
-        (reset-matrix c) ;;c is an implicit arg for the canvas
-        (doseq [[v [tx ty w h]] (map vector data coords)
-                ;:let [local-stroke (update stroke :size (:size stroke) v conf)]
-                ]
-          (filled-with-stroke c :white :black rect tx ty w h)
-          )))))
+        scale-y (partial (:scale y) 0 h)
+        coords  (mapv (fn [{:keys [x y w h label]}]
+                        {:x (scale-x x) :y (scale-y y)
+                         :w (scale-x w) :h (scale-y  h)
+                         :label label}) (map as-box data))
+        color (or color :white)
+        outline (or outline :black)]
+  (do-graph chart-data false 
+    (doseq [{:keys [x y w h label]}  coords
+            ;:let [local-stroke (update stroke :size (:size stroke) v conf)]
+            ]
+      (filled-with-stroke c color outline rect x y  w h)      
+      (when label nil)))))
+
+;;treemap expects data to come in as a tree.
+;;our job is to process it.
+(defmethod data-extent :treemap [_ data _]
+  (box-extents data))
--- src/cljplot/projectvis.clj
+++ src/cljplot/projectvis.clj
@@ -21,10 +21,119 @@
             [fastmath.kernel :as kk]
             ;;monkey patches to get this working, pending issues in repo.
             [cljplot.defaults :as defaults]
+            [cljplot.impl.treemap]
+            [cljplot.squarify :as squarify]
             [spork.util.codecount :as cc]
             ))
 
+
+(defn path->key [p]
+  (->> (spork.util.io/list-path p)
+       (into [] (comp (drop 1)
+                      (map #(symbol (clojure.string/replace % ".clj" "")))))))
+(defn dataset [ps]
+  (->>  (for [p ps
+              [k {:keys [path comment code blank] :as stats}] (-> p meta :pieces)
+              :when (clojure.string/includes? k "src")
+              ]
+          (let [s (path->key k)]
+            (assoc stats :path s :series (first s))))
+        (sort-by :path)))
+
+(defn datatree
+  ([ds depth]
+   (if (map? ds) ds
+       (let [[current deeper]
+             (reduce (fn [[c d] r]
+                       (if (> (-> r :path count) (inc depth))
+                         [c (conj d r)]
+                         [(conj c r) d]))
+                     [[] []] ds)]
+         (into current
+               (for [[g xs] (group-by #(-> % :path (nth depth)) deeper)]
+                 (let [stats (reduce (fn [l r]
+                                       {:comment (+ (:comment l 0) (:comment r 0))
+                                        :code    (+ (:code l 0) (:code r 0))
+                                        :blank   (+ (:blank l 0) (:blank r 0))})
+                                     {:comment 0 :code 0 :blank 0}
+                                     xs)] 
+                   {:path g
+                    :stats stats
+                    :children (datatree xs (inc depth))}))))))
+  ([ds] (datatree ds 0)))
+
+(defn tree->rects [root]
+  (map (fn [r]
+         (as-> {:label (if (coll? (:path r))
+                         (last (:path r))
+                         (:path r))
+                           
+                :area  (or (:stats r)
+                           (select-keys r [:code :comment :blank]))} m
+              (if (and (:children r)
+                       (> (count (:children r)) 1))
+                (assoc m :children (tree->rects (:children r)))
+                m))
+         ) root))
+
+(defn layout-tree [root x y width height & {:keys [node->area depth]
+                                            :or {node->area (fn line-count [r]
+                                                              (reduce +  (vals (:area r))))
+                                                 depth 0}}]
+  (let [sorted (sort-by (comp - node->area) (filter (comp pos? node->area) root))
+        sizes  (squarify/normalize-sizes (map node->area sorted) width height)
+        rects  (squarify/squarify sizes x y width height)]
+    (map (fn [r {:keys [x y dx dy] :as rect}]
+           (if (:children r)
+             (assoc rect
+                    :depth depth
+                    :label (:label r)
+                    :children (layout-tree (:children r) x y dx dy
+                                           :node->area node->area
+                                           :depth (inc depth)))
+             (assoc rect :depth depth :label (:label r))))
+         sorted rects)))
+
+(defn layout->rects [xs]
+  (->> (tree-seq (fn [x]
+                   (cond (map? x) (:children x)
+                         :else (coll? x)))
+                 (fn [r]
+                   (cond (map? r)   (:children r)
+                         (coll? r) r)) xs)
+       flatten
+       (map #(dissoc % :children))
+       distinct
+       (sort-by :depth)))
+
+(defn classified->boxes
+  "given a project classification p, a root x y, and
+   a containing box, w h, layouts out the project dependencies
+   returning a seq of labeled rectangles by depth
+   suitably for rendering."
+  ([p x y w h]
+   (->>  (-> p
+             dataset
+             datatree
+             tree->rects
+             (layout-tree x y w h)
+             layout->rects)
+         (map (fn [{:keys [dx dy] :as r}]
+                (assoc r :w dx :h dy)))))
+  ([p x y] (let [area (reduce + (mapcat vals (map :totals p)))
+                 half (/ area 2.0)]
+             (if (even? area)
+               (classified->boxes p 0 0 half half)
+               (classified->boxes p 0 0 (long half) (inc (long half))))))
+  ([p] (classified->boxes p 0 0)))
+
+
+(comment
+;;scripting bit
+;;Aim our thing at a couple of root folders with
+;;typical lein layout (e.g. src/blah).
 (def res (cc/classify-projects "../spork/" "../marathon/"))
+
 ;;#'codecount/res
 res
 ;;({:path "../spork/",
@@ -33,20 +142,63 @@ res
 ;;  :totals {:code 64273, :comment 25675, :blank 11720}})
 
 (def totals (reduce (partial merge-with +) (map :totals res)))
+;;convenient totals stats...
 
-(defn dataset [ps]
-  (for [p ps
-        [k {:keys [comment code blank] :as stats}] (-> p meta :pieces)]
-    (assoc stats :path k :series (:path p))))
+;;and we get back a result mapping
+;;some code stats to the path.
 
-(defn ns-children [x]
-  (some-> x meta :pieces))
+;;The metadata hides a :pieces map,
+;;which is a collection of similar statistics
+;;for all the source files traversed.
 
-(defn data-tree [root]
-  (tree-seq #(or (ns-children %) (coll? %)) #(map (fn [x] (or (seq (ns-children x)) x)) %) root))
+;;So we have the stuff we need to build a
+;;tree to map visually.
+
+;;we can use our pipeline to get
+;;cljplot friendly "boxes" of {x y w h label}
+;;that are laid out according to the
+;;squarify algorithm.
+(def rs (classified->boxes res))
+
+;;then we can visualize said boxes
+;;in various naive ways.  This is
+;;our first tree-map!
+(-> (b/series [:grid]
+              [:boxes rs {:color :white}])
+    (b/preprocess-series)
+    (b/add-axes  :bottom)
+    (b/add-axes  :left) 
+    (r/render-lattice {:width 1000 :height 1000})
+    (save "primitivetree.png")
+    (show))
+)
+
+(comment
+  ;;Another simpler example, this time with cljplot itself.
+(-> (b/series [:grid]
+              [:boxes (-> (cc/classify-projects "../cljplot/" "../fastmath/")
+                          classified->boxes) {:color :blue :outline :white}])
+    (b/preprocess-series)
+    (b/add-axes  :bottom)
+    (b/add-axes  :left  )  
+    (r/render-lattice {:width 1000 :height 1000})
+    (save "cljplot.png")
+    (show))
+)
+
+
+
+
+
+;;Back Burner
+;;===========
+
+
+
+
 
 ;;styling either by a 
-(defn with-styling [m & xs]
+#_(defn with-styling [m & xs]
   (for [[idx x] (map-indexed vector xs)]
     (let [style (nth x 2 {:series-id idx})
           new-style (or (some-> style :series-label m)
@@ -54,7 +206,7 @@ res
       (assoc x 2 (merge style (or (some-> style :series-label m)
                                   new-style))))))
 
-(let [p       (c/palette-presets :tableau-10-2)
+#_(let [p       (c/palette-presets :tableau-10-2)
       styling [{:series-label "blue"   :color (p 0) :shape \o}
                {:series-label "orange" :color (p 1)}]]
   (-> (b/series [:grid])
@@ -66,13 +218,33 @@ res
       (r/render-lattice {:with 1080 :height 720})
       (show)))
 
+#_(-> (b/series [:grid]
+              [:scatter (relative-rect 0 0 613.284433577832 1000.0) {:color :red}]
+              [:scatter (relative-rect 0 0 386.71556642216797 999.9999999999998) {:color :blue}])
+                       
+    (b/preprocess-series)
+    (defaults/add-basic-styling)
+    (r/render-lattice {:with 1000 :height 1000})
+    (show))
 
-(let [p       (c/palette-presets :tableau-10-2)
-      styling [{:series-label "blue"   :color (p 0) :shape \o}
-               {:series-label "orange" :color (p 1)}]]
-  (-> (b/series [:grid]
-                [:tree-map (mapv (fn [x] [x (rand-int 50) (rand-int 100) (rand-int 100)])  (range 100))])
+;;playing with treemaps
+
+#_(-> (b/series [:grid]
+              [:boxes [[0 0 100 100] [500 500 100 100]]])
       (b/preprocess-series)
-      ;(defaults/add-basic-styling)
-      (r/render-lattice {:with 1080 :height 720})
-      (show)))
+      (b/add-axes  :bottom)
+      (b/add-axes  :left) 
+      (r/render-lattice {:width 1000 :height 1000})
+      (show))
+
+
+#_(-> (b/series [:grid]
+               [:scatter (repeatedly 1000 (fn [] [(rand-int 100) (rand-int 100)]))
+                {:color :red}]
+               [:scatter (repeatedly 1000 (fn [] [(+ 100 (rand-int 100)) (+ 100 (rand-int 100))]))
+                {:color :blue}])
+     (b/preprocess-series)
+     (b/add-axes  :bottom)
+     (b/add-axes  :left)
+     (r/render-lattice {:width 1000 :height 1000})
+     (show))
